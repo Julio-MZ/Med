3c9db470853d6d0acf9afba4be819fc8d5c5c5df
Julio <julio_munoz_03@hotmail.com>
2022-06-18 23:17:45 -0400

    v4

--- app/src/main/java/com/example/med/Inicio.java
+++ app/src/main/java/com/example/med/Inicio.java
@@ -2,11 +2,16 @@ package com.example.med;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.med.Conexion.ConexionSQLiteHelper;
 
 public class Inicio extends AppCompatActivity {
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,12 @@ public class Inicio extends AppCompatActivity {
         setContentView(R.layout.activity_inicio);
 
         ConexionSQLiteHelper con = new ConexionSQLiteHelper(this, "BD_Med", null, 1);
+
+        button2 = (Button) findViewById(R.id.button2);
+    }
+
+    public void button2(View view) {
+        Intent i = new Intent(this, Registro.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/med/Registro.java
@@ -0,0 +1,51 @@
+package com.example.med;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Calendar;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+    EditText editTextDate;
+
+    private int dia, mes, anio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.generos_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        editTextDate = (EditText) findViewById(R.id.editTextDate);
+
+        editTextDate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        final Calendar c = Calendar.getInstance();
+        dia = c.get(Calendar.DAY_OF_MONTH);
+        mes = c.get(Calendar.MONTH);
+        anio = c.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                editTextDate.setText(day + "/" + (month + 1) + "/" + year);
+            }
+        }, dia, mes, anio);
+        datePickerDialog.show();
+    }
+}
